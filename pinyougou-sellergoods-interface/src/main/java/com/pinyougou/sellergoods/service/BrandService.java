18c67402582fc4b0428c0769e747b58c59c8e67a
xiefn <xfn7311@163.com>
2018-03-15 12:17:35 +0800

    完成品牌

--- pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -11,17 +11,41 @@ import com.pinyougou.pojo.TbBrand;
  */
 public interface BrandService {
 	
-	List<TbBrand> findAll();
+	public List<TbBrand> findAll();
 	/**
 	 * 返回分页列表
 	 * @param pageNum
 	 * @param pageSize
 	 * @return
 	 */
-	PageResult findPage(int pageNum , int pageSize);
+	public PageResult findPage(int pageNum , int pageSize);
 	/**
 	 * 添加品牌
 	 * @param brand
 	 */
-	void add(TbBrand brand);
+	public void add(TbBrand brand);
+	/**
+	 * 修改品牌
+	 * @param brand
+	 */
+	public void update(TbBrand brand);
+	/**
+	 * 根据id查询品牌实体
+	 * @param id
+	 * @return
+	 */
+	public TbBrand findOne(Long id);
+	/**
+	 * 根据id批量删除品牌
+	 * @param ids
+	 */
+	public void delete(Long[] ids);
+	/**
+	 * 带条件分页查询
+	 * @param brand
+	 * @param pageNum
+	 * @param pageSize
+	 * @return
+	 */
+	public PageResult findPage(TbBrand brand,int pageNum , int pageSize);
 }
--- pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -10,26 +10,68 @@ import com.github.pagehelper.PageHelper;
 import com.pinyougou.entity.PageResult;
 import com.pinyougou.mapper.TbBrandMapper;
 import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.pojo.TbBrandExample;
+import com.pinyougou.pojo.TbBrandExample.Criteria;
 import com.pinyougou.sellergoods.service.BrandService;
+
 @Service
 public class BrandServiceImpl implements BrandService {
 
 	@Autowired
 	private TbBrandMapper brandMapper;
+
 	@Override
 	public List<TbBrand> findAll() {
-		
+
 		return brandMapper.selectByExample(null);
 	}
+
 	@Override
 	public PageResult findPage(int pageNum, int pageSize) {
 		PageHelper.startPage(pageNum, pageSize);
 		Page<TbBrand> page = (Page<TbBrand>) brandMapper.selectByExample(null);
 		return new PageResult(page.getTotal(), page.getResult());
 	}
+
 	@Override
 	public void add(TbBrand brand) {
 		brandMapper.insert(brand);
+	}
+
+	@Override
+	public void update(TbBrand brand) {
+		brandMapper.updateByPrimaryKey(brand);
+	}
+
+	@Override
+	public TbBrand findOne(Long id) {
+		return brandMapper.selectByPrimaryKey(id);
+
+	}
+
+	@Override
+	public void delete(Long[] ids) {
+		for (Long id : ids) {
+			brandMapper.deleteByPrimaryKey(id);
+		}
+	}
+
+	@Override
+	public PageResult findPage(TbBrand brand, int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);// 分页插件
+		TbBrandExample example = new TbBrandExample();
+		Criteria criteria = example.createCriteria();
+		if (brand != null) {
+			if (brand.getName() != null && brand.getName().length() > 0) {
+				criteria.andNameLike("%" + brand.getName() + "%");
+			}
+			if (brand.getFirstChar() != null && brand.getFirstChar().length() > 0) {
+				criteria.andFirstCharEqualTo(brand.getFirstChar().toUpperCase());
+			}
+		}
+
+		Page<TbBrand> page = (Page<TbBrand>) brandMapper.selectByExample(example);
+		return new PageResult(page.getTotal(), page.getResult());
 	}
 
 }
